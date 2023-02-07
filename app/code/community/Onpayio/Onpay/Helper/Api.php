0fd832f13268df42b053c33a4fc7f76aeafe8121
ebbesmoeller <esm@zitcom.dk>
2023-02-07 12:38:49 +0100

    Added functions for authenticating with OnPay API

--- app/code/community/Onpayio/Onpay/Helper/Api.php
+++ app/code/community/Onpayio/Onpay/Helper/Api.php
@@ -17,7 +17,10 @@ class Onpayio_Onpay_Helper_Api extends Mage_Core_Helper_Abstract {
 
     public function getWindowDesigns() {
         $onpayApi = $this->getOnPayClient();
-        return $onpayApi->gateway()->getPaymentWindowDesigns();
+        if ($onpayApi->isAuthorized()) {
+            return $onpayApi->gateway()->getPaymentWindowDesigns();
+        }
+        return [];
     }
 
     public function isReturnQueryValid(array $query) {
@@ -26,12 +29,51 @@ class Onpayio_Onpay_Helper_Api extends Mage_Core_Helper_Abstract {
         return $paymentWindow->validatePayment($query);
     }
 
-    private function getOnPayClient() {
+    public function getAuthUrl() {
+        $onpayApi = $this->getOnPayClient(true);
+        return $onpayApi->authorize();
+    }
+
+    public function finishAuthSetup(array $query) {
+        $onpayApi = $this->getOnPayClient(true);
+        if(array_key_exists('code', $query) && !$onpayApi->isAuthorized()) {
+            // We're not authorized with the API, and we have a 'code' value at hand. 
+            // Let's authorize, and save the gatewayID and secret accordingly.
+            $onpayApi->finishAuthorize($query['code']);
+            if ($onpayApi->isAuthorized()) {
+                $this->setSetting('gateway', $onpayApi->gateway()->getInformation()->gatewayId);
+                $this->setSetting('secret', $onpayApi->gateway()->getPaymentWindowIntegrationSettings()->secret);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function detachAuth() {
+        $this->deleteSetting('gateway');
+        $this->deleteSetting('secret');
+        $this->deleteSetting('oauth2_token');
+    }
+
+    
+    public function isConnected() {
+        $onpayApi = $this->getOnPayClient();
+        return $onpayApi->isAuthorized();
+    }
+
+    private function getOnPayClient($prepareRedirectUri = false) {
         $accessToken = Mage::getStoreConfig('payment/onpay/apikey');
-        $tokenStorage = new \OnPay\StaticToken($accessToken);
-        $onPayAPI = new OnPayAPI($tokenStorage, [
-            'client_id' => 'Static token',
-        ]);
+        $tokenStorage = Mage::getModel('onpay/TokenStorage');
+
+        $params = [
+            'client_id' => 'OnPay OpenMage',
+            'redirect_uri' => ''
+        ];
+        if ($prepareRedirectUri) {
+            $params['redirect_uri'] = Mage::helper('adminhtml')->getUrl('adminhtml/onpayAuth/return', ['_secure' => true]);
+        }
+        $onPayAPI = new OnPayAPI($tokenStorage, $params);
+        
         return $onPayAPI;
     }
 
@@ -86,5 +128,13 @@ class Onpayio_Onpay_Helper_Api extends Mage_Core_Helper_Abstract {
 
     private function getSetting($setting, $entity = 'onpay') {
         return Mage::getStoreConfig('payment/' . $entity . '/' . $setting);
+    }
+
+    private function setSetting($setting, $value, $entity = 'onpay') {
+        return Mage::getConfig()->saveConfig('payment/' . $entity . '/' . $setting, $value);
+    }
+
+    private function deleteSetting($setting, $entity = 'onpay') {
+        return Mage::getConfig()->deleteConfig('payment/' . $entity . '/' . $setting);
     }
 }
