ea17e945f2c814b912fda026a7db1631f608136f
ebbesmoeller <esm@zitcom.dk>
2023-02-08 16:16:29 +0100

    Added remaining methods available in SDK

--- app/code/community/Onpayio/Onpay/Model/Method/Abstract.php
+++ app/code/community/Onpayio/Onpay/Model/Method/Abstract.php
@@ -2,6 +2,9 @@
 
 abstract class Onpayio_Onpay_Model_Method_Abstract extends Mage_Payment_Model_Method_Abstract {
     protected $_isInitializeNeeded = true;
+    protected $_canCapture = true;
+    protected $_canRefund = true;
+    protected $_canRefundInvoicePartial = true;
     protected $_canUseInternal = false;
     protected $_canUseForMultishipping = false;
 
@@ -9,5 +12,9 @@ abstract class Onpayio_Onpay_Model_Method_Abstract extends Mage_Payment_Model_Me
 
     public function getOrderPlaceRedirectUrl() {
         return Mage::helper('onpay/api')->getPaymentLink($this->getMethod());
+    }
+
+    public function canUseForCurrency($currencyCode) {
+        return Mage::helper('onpay/currency')->isCurrencySupported($currencyCode, $this->getMethod());
     }
 }
---
+++ app/code/community/Onpayio/Onpay/Model/Method/Viabill.php
@@ -0,0 +1,12 @@
+<?php
+
+use OnPay\API\Util\PaymentMethods\Enums\Methods;
+
+class Onpayio_Onpay_Model_Method_Viabill extends Onpayio_Onpay_Model_Method_Abstract {
+    const METHOD_CODE = 'onpay_viabill';
+    protected $_code = self::METHOD_CODE;
+
+    protected function getMethod() {
+        return Methods::VIABILL;
+    }
+}
